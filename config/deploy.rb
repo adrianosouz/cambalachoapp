fa5f0bfa56896e1fdf381212b51e42769eec6cd1
adrianosouz <adrianopereiradesousa81@gmail.com>
2018-03-19 18:21:44 +0000

    Mais uma vez

--- config/deploy.rb
+++ config/deploy.rb
@@ -10,7 +10,6 @@ set :repo_url, "https://github.com/adrianosouz/cambalachoapp.git"
 # Default deploy_to directory is /var/www/my_app_name
  set :deploy_to, "/var/www/cambalachoapp"
 
-
  set :branch, "master"
 # Default value for :format is :airbrussh.
   set :format, :airbrussh
